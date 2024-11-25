6e3e9c95f9e89fef8b666e75ee10fb932b632a4f
victor1123-alt <mkvictor579@gmail.com>
2024-11-25 13:24:24 +0100

    modeified connection at models/index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const dbConfig = require("../config/db.config.js");
+const url = "mysql://root:cYYWexYqmCzLvhrWdCVTAssMphfVknZP@mysql.railway.internal:3306/railway"
 
-
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+const sequelize = new Sequelize(url, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
 });
