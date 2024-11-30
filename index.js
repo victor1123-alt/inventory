4942089c96d9fa380e212b55d360f9470837be54
victor1123-alt <mkvictor579@gmail.com>
2024-11-30 15:39:29 +0100

    trying to sync my db with hack pattern

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/customers', verifyCookies,customerRoutes);
 app.use('/api/users',userRoutes);
 
 // Sync database and start server
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
